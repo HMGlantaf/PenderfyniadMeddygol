e3163ba5f4c272ca7ec1bef7fdd0049c4c7c9031
HMGlantaf <hm@glantaf.cardiff.sch.uk>
2016-05-03 14:15:49 +0100

    Ychwanegu sylw am wrthod

--- prjPendrfyniadMeddygol/frmSefyllfa1.vb
+++ prjPendrfyniadMeddygol/frmSefyllfa1.vb
@@ -1,5 +1,16 @@
 ﻿Public Class frmSefyllfa1
 
+    'Cyfrif y nifer o gleifion
+    Dim Cyfanswm1 As Integer = 0
+    Dim Cyfanswm2 As Integer = 0
+    Dim Cyfanswm3 As Integer = 0
+    Dim Cyfanswm4 As Integer = 0
+    'Cyfrif y nifer sy'n cael gwrthod llawdriniaeth
+    Dim Gwrthod1 As Integer = 0
+    Dim Gwrthod2 As Integer = 0
+    Dim Gwrthod3 As Integer = 0
+    Dim Gwrthod4 As Integer = 0
+
     Private Sub frmSefyllfa1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Gosod gwerthoedd cychwynol y blwch
         chk_A_PwysauNormal.Checked = False
@@ -14,24 +25,35 @@
         Dim B As Boolean = False
         Dim C As Boolean = False
 
+        'Rheswm am wrthod llawdriniaeth
+        Dim Rheswm As String = "Rheswm am wrthod llawdriniaeth yw:"
+
         'Gwerth cychwynol triniaeth
         Dim Triniaeth As Boolean = False
 
         'Enw'r claf
         Dim Enw As String = ""
 
+        'Canran gwrthod llawdriniaeth
+
         'Darllen mewnbwn
         Mewnbwn(Enw, A, B, C)
 
+        'Ychwanegu at y cyfanswm
+        Cyfanswm1 = Cyfanswm1 + 1
         'Penderfynu os yw'n gallu derbyn triniaeth
         If A Or B Or C Then 'gallu
             Triniaeth = True
         Else 'Dim yn gallu
             Triniaeth = False
+            'Darganfod y rheswm am wrthod
+            Y_Rheswm(Rheswm, A, B, C)
+            'Ychwanegu at y nifer sy'n cael ei gwrthod
+            Gwrthod1 = Gwrthod1 + 1
         End If
 
         'Allbynnu'r penderfyniad
-        Allbwn(Enw, Triniaeth)
+        Allbwn(Enw, Triniaeth, Rheswm)
 
     End Sub
 
@@ -42,6 +64,9 @@
         Dim A As Boolean = False
         Dim B As Boolean = False
         Dim C As Boolean = False
+
+        'Rheswm am wrthod llawdriniaeth
+        Dim Rheswm As String = "Rheswm am wrthod llawdriniaeth yw:"
 
         'Gwerth cychwynol triniaeth
         Dim Triniaeth As Boolean = False
@@ -57,10 +82,12 @@
             Triniaeth = True
         Else 'Dim yn gallu
             Triniaeth = False
+            'Darganfod y rheswm am wrthod
+            Y_Rheswm(Rheswm, A, B, C)
         End If
 
         'Allbynnu'r penderfyniad
-        Allbwn(Enw, Triniaeth)
+        Allbwn(Enw, Triniaeth, Rheswm)
     End Sub
 
     Private Sub btnSefyllfa3_Click(sender As Object, e As EventArgs) Handles btnSefyllfa3.Click
@@ -70,6 +97,9 @@
         Dim A As Boolean = False
         Dim B As Boolean = False
         Dim C As Boolean = False
+
+        'Rheswm am wrthod llawdriniaeth
+        Dim Rheswm As String = "Rheswm am wrthod llawdriniaeth yw:"
 
         'Gwerth cychwynol triniaeth
         Dim Triniaeth As Boolean = False
@@ -86,10 +116,12 @@
             Triniaeth = True
         Else 'Dim yn gallu
             Triniaeth = False
+            'Darganfod y rheswm am wrthod
+            Y_Rheswm(Rheswm, A, B, C)
         End If
 
         'Allbynnu'r penderfyniad
-        Allbwn(Enw, Triniaeth)
+        Allbwn(Enw, Triniaeth, Rheswm)
     End Sub
 
     Private Sub btnSefyllfa4_Click(sender As Object, e As EventArgs) Handles btnSefyllfa4.Click
@@ -99,6 +131,9 @@
         Dim A As Boolean = False
         Dim B As Boolean = False
         Dim C As Boolean = False
+
+        'Rheswm am wrthod llawdriniaeth
+        Dim Rheswm As String = "Rheswm am wrthod llawdriniaeth yw:"
 
         'Gwerth cychwynol triniaeth
         Dim Triniaeth As Boolean = False
@@ -115,10 +150,12 @@
             Triniaeth = True
         Else 'Dim yn gallu
             Triniaeth = False
+            'Darganfod y rheswm am wrthod
+            Y_Rheswm(Rheswm, A, B, C)
         End If
 
         'Allbynnu'r penderfyniad
-        Allbwn(Enw, Triniaeth)
+        Allbwn(Enw, Triniaeth, Rheswm)
     End Sub
 
     Private Sub Mewnbwn(ByRef Enw As String, ByRef A As Boolean, ByRef B As Boolean, ByRef C As Boolean)
@@ -133,14 +170,35 @@
 
     End Sub
 
-    Private Sub Allbwn(ByVal Enw As String, ByVal Triniaeth As Boolean)
+    Private Sub Allbwn(ByVal Enw As String, ByVal Triniaeth As Boolean, ByVal Rheswm As String)
         'Allbynnu'r penderfyniad
         lblPenderfyniad.Text = "Penderfyniad: "
         If Triniaeth Then 'Gallu derbyn triniaeth
             lblPenderfyniad.Text = lblPenderfyniad.Text & "Mae " & Enw & " yn gallu derbyn triniaeth"
         Else 'Dim yn gallu derbyn triniaeth
             lblPenderfyniad.Text = lblPenderfyniad.Text & "Nid yw " & Enw & " yn gallu derbyn triniaeth"
+            'Allbynnu'r rheswm
+            lblPenderfyniad.Text = lblPenderfyniad.Text & vbCrLf & Rheswm
         End If
     End Sub
+
+    Private Sub Y_Rheswm(ByRef Rheswm As String, ByVal A As Boolean, ByVal B As Boolean, ByVal C As Boolean)
+        'Rhesymau am wrthod llawdriniaeth
+        Dim RheswmA As String = "Pwysau uchel"
+        Dim RheswmB As String = "Ysmygu"
+        Dim RheswmC As String = "Yfed yn uchel"
+
+        'Ychwanegu rhesymau
+        If Not (A) Then
+            Rheswm = Rheswm & vbCrLf & RheswmA
+        End If
+        If Not (B) Then
+            Rheswm = Rheswm & vbCrLf & RheswmB
+        End If
+        If Not (C) Then
+            Rheswm = Rheswm & vbCrLf & RheswmC
+        End If
+    End Sub
+
 
 End Class
